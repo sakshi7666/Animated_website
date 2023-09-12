b074203902c48e714d830115d3f760518bdc5d04
Sakshi <sakshinimbore2020.comp@mmcoe.edu.in>
2023-09-12 15:53:36 +0530

    Update Cusrsor size

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var blur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function(dets){
     crsr.style.left = dets.x+"px"
     crsr.style.top = dets.y+"px"
-    blur.style.left = dets.x-200+"px"
-    blur.style.top = dets.y-200+"px"
+    blur.style.left = dets.x-100+"px"
+    blur.style.top = dets.y-100+"px"
 })
 
 
